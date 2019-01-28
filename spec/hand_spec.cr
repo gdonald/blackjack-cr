b6110edd7bd2cb332ceb9f9b3775ac924f7ff471
Greg Donald <gdonald@gmail.com>
2019-01-27 21:46:11 -0600

    more specs

---
+++ spec/hand_spec.cr
@@ -0,0 +1,14 @@
+require "spec"
+require "../lib/hand"
+
+describe Hand do
+  it "has Status enum" do
+    expected = ["Unknown", "Won", "Lost", "Push"]
+    Hand::Status.names.should eq(expected)
+  end
+
+  it "has Count enum" do
+    expected = ["Soft", "Hard"]
+    Hand::Count.names.should eq(expected)
+  end
+end 
